bfd5a89093c716d62d269b296a780c74c010d37f
Yingying <yingying54319@yahoo.com>
2018-03-02 11:31:08 -0800

    Add files via upload

---
+++ project/int-wireframe/script.js
@@ -0,0 +1,53 @@
+/* === studio javascript == */
+
+function initMap() {
+  // Map options
+  var options = {
+    zoom: 2,
+    center: {
+      lat: 25.9631,
+      lng: -1.0208
+    }
+  }
+
+  // New map
+  var map = new google.maps.Map(document.getElementById('map'), options);
+
+  // Listen for click on map
+  google.maps.event.addListener(map, 'click', function(event) {
+    // Add marker
+    addMarker({
+      coords: event.latLng
+    });
+  });
+
+
+
+  // Array of markers
+
+  // Loop through markers
+  for (var i = 0; i < markers.length; i++) {
+    // Add marker
+    addMarker(markers[i]);
+  }
+
+  // Add Marker Function
+  function addMarker(props) {
+    var marker = new google.maps.Marker({
+      position: props.coords,
+      map: map,
+      //icon:props.iconImage
+    });
+
+    // Check content
+    if (props.content) {
+      var infoWindow = new google.maps.InfoWindow({
+        content: props.content
+      });
+
+      marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+      });
+    }
+  }
+}
