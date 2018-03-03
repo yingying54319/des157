ff4e0b170a0ce316ad60b48662af60e3c12f3dc4
Yingying <yingying54319@yahoo.com>
2018-03-02 19:04:02 -0800

    Add files via upload

---
+++ project/Interactive Prototype 1/script.js
@@ -0,0 +1,61 @@
+/* === studio javascript == */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function initMap() {
+  // Map options
+  var options = {
+    zoom: 3,
+    center: {
+      lat: 34.0479,
+      lng: 100.6197
+    }
+  }
+
+  // New map
+  var map = new google.maps.Map(document.getElementById('map'), options);
+
+  // Listen for click on map
+  google.maps.event.addListener(map, 'click', function(event) {
+    // Add marker
+    addMarker({
+      coords: event.latLng
+    });
+  });
+
+
+
+  // Array of markers
+
+  // Loop through markers
+  for (var i = 0; i < markers.length; i++) {
+    // Add marker
+    addMarker(markers[i]);
+  }
+
+  // Add Marker Function
+  function addMarker(props) {
+    var marker = new google.maps.Marker({
+      position: props.coords,
+      map: map,
+      //icon:props.iconImage
+    });
+
+    // Check content
+    if (props.content) {
+      var infoWindow = new google.maps.InfoWindow({
+        content: props.content
+      });
+
+      marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+      });
+    }
+  }
+}
